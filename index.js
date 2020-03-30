d0f6aec3a529c7121e2d2f88918a2969d1a990d7
Damian Pioś <damian.pios@infinite.pl>
2020-03-30 09:49:45 +0200

    working release

--- index.js
+++ index.js
@@ -1,29 +1,65 @@
+#!/usr/bin/env node
+
 const { exec } = require('child_process');
 const fs = require('fs');
 
 const main = () => {
-    exec('npm i --save-dev jest jest-preset-angular');
-    exec('npm uninstall karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter');
-    fs.writeFile('src/jestGlobalMock.ts', '', err => {
-        if (err) {
-            console.error('Problem with creating file jestGlobalMock.ts');
-            console.error(err);
-            throw err;
-        }
-        console.log('File jestGlobalMock.ts created');
-    });
-    fs.writeFile('src/setupJest.ts', "import 'jest-preset-angular'\;\nimport './jestGlobalMock'\;", err => {
-        if (err) {
-            console.error('Problem with creating file setupJest.ts');
-            console.error(err);
-            throw err;
-        }
-        console.log('File jestGlobalMock.ts created');
-    });
+    execCommands().then(() => {
+        createFiles();
 
-    const package = JSON.parse(fs.readFileSync('package.json', 'UTF-8'));
-    console.log(package);
-    package.jest = {
+        updatePackage();
+
+        updateAngular();
+
+        deleteFiles();
+
+        updateTsConfigSpec();
+    });
+}
+
+const execCommands = async () => {
+    return execCommandPromise('npm i --save-dev jest jest-preset-angular')
+        .then(() => execCommandPromise('npm uninstall karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter'))
+};
+
+const execCommandPromise = async (command) => {
+    return new Promise((res, rej) => {
+        const child = exec(command, (err, stdout, stderr) => {
+            if (err) {
+                console.error(`exec error: ${err}`);
+                rej(err);
+            }
+            console.error(stderr);
+            res();
+        });
+
+        child.stdout.pipe(process.stdout);
+    });
+}
+
+const createFiles = () => {
+    try {
+        fs.writeFileSync('src/jestGlobalMock.ts', '');
+        console.log('File jestGlobalMock.ts created');
+    } catch (err) {
+        console.error('Problem with creating file jestGlobalMock.ts');
+        console.error(err);
+        throw err;
+    }
+
+    try {
+        fs.writeFileSync('src/setupJest.ts', "import 'jest-preset-angular'\; \n import './jestGlobalMock'\;");
+        console.log('File setupJest.ts created');
+    } catch (err) {
+        console.error('Problem with creating file setupJest.ts');
+        console.error(err);
+        throw err;
+    }
+};
+
+const updatePackage = () => {
+    const packageJson = JSON.parse(fs.readFileSync('package.json', 'UTF-8'));
+    packageJson.jest = {
         preset: "jest-preset-angular",
         setupFilesAfterEnv: [
             "<rootDir>/src/setupJest.ts"
@@ -35,69 +71,84 @@ const main = () => {
             "<rootDir>/src/test.ts"
         ]
     }
-    package.scripts.test = 'jest';
-    fs.writeFile('package.json', JSON.stringify(package, null, 2), err => {
-        if (err) {
-            console.error('Problem with updating file package.json');
-            console.log('You can do it by yourself');
-            console.log('Just add following to package.json');
-            console.log(`jest: ${JSON.stringify(package.jest, null, 2)}`);
-            console.log('And change scripts.test to "jest"');
-            console.error(err);
-        }
-        console.log('File package.json updated');
-    });
+    packageJson.scripts.test = 'jest';
 
+    try {
+        fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+        console.log('File package.json updated');
+    } catch (err) {
+        console.error('Problem with updating file package.json');
+        console.log('You can do it by yourself');
+        console.log('Just add following to package.json');
+        console.log(`jest: ${JSON.stringify(packageJson.jest, null, 2)}`);
+        console.log('And change scripts.test to "jest"');
+        console.error(err);
+
+    }
+};
+
+const updateAngular = () => {
     const angular = JSON.parse(fs.readFileSync('angular.json', 'UTF-8'));
 
     const keys = Object.keys(angular.projects);
     keys.forEach(key => {
-        angular.projects[key].architecture.test = undefined;
-    });
+        try {
+            angular.projects[key].architecture.test = undefined;
+        } catch (e) {
 
-    fs.writeFile('angular.json', JSON.stringify(angular, null, 2), err => {
-        if (err) {
-            console.error('Problem with updating file angular.json');
-            console.log('You can do it by yourself');
-            console.log('Just remove test section from all projects');
-            console.error(err);
         }
+    });
+    try {
+        fs.writeFileSync('angular.json', JSON.stringify(angular, null, 2));
         console.log('File angular.json updated');
-    });
+    } catch (err) {
+        console.error('Problem with updating file angular.json');
+        console.log('You can do it by yourself');
+        console.log('Just remove test section from all projects');
+        console.error(err);
 
-    fs.unlink('src/karma.config.js', err => {
-        if (err) {
-            console.error('Problem with deleting file src/karma.config.js');
-            console.error(err);
-        }
-        console.log('File src/karma.config.js updated');
-    });
+    }
+};
 
-    fs.unlink('src/test.ts', err => {
-        if (err) {
-            console.error('Problem with deleting file src/test.ts');
-            console.error(err);
-        }
-        console.log('File src/test.ts updated');
-    });
+const deleteFiles = () => {
+    try {
+        fs.unlinkSync('karma.conf.js');
+        console.log('File src/karma.config.js deleted');
+    } catch (err) {
+        console.error('Problem with deleting file src/karma.config.js');
+        console.error(err);
+    }
 
+    try {
+        fs.unlinkSync('src/test.ts');
+        console.log('File src/test.ts deleted');
+    } catch (err) {
+        console.error('Problem with deleting file src/test.ts');
+        console.error(err);
+    }
+};
+
+const updateTsConfigSpec = () => {
     const tsconfigSpec = JSON.parse(fs.readFileSync('tsconfig.spec.json', 'UTF-8'));
 
     const specFiles = tsconfigSpec.files;
+    tsconfigSpec.compilerOptions.sModuleInterop = true;
 
     const index = specFiles.findIndex(file => file === 'src/test.ts');
     if (index > -1) {
         specFiles.splice(index, 1);
-        fs.writeFile('tsconfig.spec.json', JSON.stringify(tsconfigSpec, null, 2), err => {
-            if (err) {
-                console.error('Problem with updating file tsconfig.spec.json');
-                console.log('You can do it by yourself');
-                console.log('Just remove "src/test.ts" from files section');
-                console.error(err);
-            }
+        try {
+            fs.writeFileSync('tsconfig.spec.json', JSON.stringify(tsconfigSpec, null, 2));
             console.log('File tsconfig.spec.json updated');
-        });
+        } catch (err) {
+            console.error('Problem with updating file tsconfig.spec.json');
+            console.log('You can do it by yourself');
+            console.log('Just remove "src/test.ts" from files section');
+            console.log('And add "sModuleInterop": true to compiler options');
+            console.log('In tsconfig.spec.json');
+            console.error(err);
+        }
     }
-}
+};
 
 main();
